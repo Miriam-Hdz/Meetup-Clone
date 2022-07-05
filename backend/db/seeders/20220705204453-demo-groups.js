e561b41415ad03e4a84b7941de3793db8587f8b3
miriam <miriam23_hernandez@yahoo.com>
2022-07-05 16:31:35 -0500

    seed data exists in db for groups to be returned

---
+++ backend/db/seeders/20220705204453-demo-groups.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Groups', [
+      {
+        name: "Evening Tennis on the Water",
+        about: "Enjoy rounds of tennis with a tight-nit group of people on the water facing the Brooklyn Bridge. Singles or doubles.",
+        type: "In person",
+        private: true,
+        city: "New York",
+        state: "NY",
+        num_members: 10,
+        preview_image: "image url",
+        organizer_id: 1,
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Groups', null, {});
+  }
+};
