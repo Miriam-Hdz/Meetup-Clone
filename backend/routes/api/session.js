26e749931e74cdd5ec75de2d55fda9752323754c
miriam <miriam23_hernandez@yahoo.com>
2022-07-08 10:38:14 -0500

    add post route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -79,4 +79,17 @@ router.get('/groups', async (req, res) => {
 
 });
 
+router.post('/groups', async (req, res) => {
+  const { name, about, type, private, city, state } = req.body;
+  const newGroup = await Group.create({
+    name: name,
+    about: about,
+    type: type,
+    private: private,
+    city: city,
+    state: state
+  });
+});
+
+
 module.exports = router;
